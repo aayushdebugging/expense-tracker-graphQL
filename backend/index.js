f1dd8fab2940bcd151e99f262bba67fbaae308f6
AAYUSH KUMAR <121863336+aayushdebugging@users.noreply.github.com>
2024-07-01 01:38:18 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
- import dotenv from "dotenv";
-// import path from "path";
-// import passport from "passport";
-// import session from "express-session";
-// import connectMongo from "connect-mongodb-session";
+import dotenv from "dotenv";
+import path from "path";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
 
 
 import { ApolloServer } from '@apollo/server';
@@ -13,13 +13,45 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
+import { buildContext } from "graphql-passport";
+
 import mergedResolvers from './resolvers/index.js';
 import mergedTypeDefs from './typeDefs/index.js';
 
+import { connectDB } from "./db/db.js";
+import { configurePassport } from "./passport/passport.config.js";
+
+
 dotenv.config();
+configurePassport();
 const app = express()
 
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  url : process.env.MONGO_URL,
+  collection :"session",
+})
+
+store.on("error",(err)=>console.log(err));
+
+app.use(
+  session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+      maxAge:1000*60*60*24*7,
+      httpOnly:true
+    },
+    store:store
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs:mergedTypeDefs,
@@ -30,12 +62,16 @@ await server.start();
 
 app.use(
   '/',
-  cors(),
+  cors({
+    origin:"http://localhost:3000",
+    credential:true
+  }),
   express.json(),
   expressMiddleware(server,{
-    context:async({req})=>({req}),
+    context:async({req,res})=>buildContext({req,res}),
   }),
 );
 
 await new Promise((resolve) => httpServer.listen({port:4000},resolve));
+await connectDB();
 console.log(`🚀 Server ready at http://localhost:4000/`);
